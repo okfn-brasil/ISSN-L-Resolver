6b5f5e2d1925b573f12d9bea1bda1bb12f843f47
Peter <ppkrauss@gmail.com>
2014-08-19 07:16:13 -0300

    ops, translating, review comments

--- issnltables2sql.php
+++ issnltables2sql.php
@@ -1,24 +1,20 @@
 <?php
 /**
- * Conversor de arquivo ISSN-to-ISSNL para SQL PostgresSQL.
+ * ISSN-to-ISSNL file conversor, PostgresSQL tested implementation.
+ * 
  * v1.0-2014 of https://github.com/ppKrauss/ISSN-L-resolver 
- * Usar em modo shell:
- * % php issnltables2sql.php | more
- * % php issnltables2sql.php tudo | psql -h localhost -U postgres base
+ * Use at terminal:
+ *  % unzip issnltables.zip
+ *  % php issnltables2sql.php | more
+ *  % php issnltables2sql.php ALL | psql -h localhost -U postgres base
+ * Cost at postgreSQL: ~ 101700 blocks ok 1k (df command). 
  */
-
-/* Utilização de dados no postgreSQL: 36205156-36103452= 101704 blocos de 1k,
-   comando df antes e depois:
-	Sist. Arq.     1K-blocos    Usado Disponível 
-	/dev/sda6       83258952 42824420   36205156 
-	/dev/sda6       83258952 42926124   36103452 
-*/
 function echo_fileFiltered(
-		$folder='issnltables',  // noma da pasta do unzip issnltables.zip
-		$MAX = 200,             // 0 (indica TUDO) ou numero de itens desejado para amostra.
-		$table='lib.issn_l',  // nome da tabela que receberá os dados
-		$frag=90000             // numero de dados por fragmento de INSERT  (important para nao estourar memoria do psql) 
-		) {
+	$folder='issnltables',  // name of the folder produced by    unzip issnltables.zip
+	$MAX = 200,             // 0 (for ALL) or number of items for sample.
+	$table='lib.issn_l',    // name of the table that will be the recipient of "echoated data"
+	$frag=90000             // number of records per fragment, important to avoid some "psql overflow" 
+) {
 	$file='';
 	echo "\n---debug $folder\n";
 	foreach(scandir($folder) as $f)
@@ -26,10 +22,10 @@ function echo_fileFiltered(
 	$handle = fopen($file, "r");
 	$sep = '';
 	if ($handle) {
-		echo "\nDROP TABLE IF EXISTS $table;";
-		echo "\nCREATE TABLE $table (issn int not null primary key, issn_l int not null);";
-		echo "\nINSERT INTO $table (issn, issn_l) VALUES ";
-	    for($n=0; (!$MAX || $n<$MAX) && ($line = fgets($handle, 4096)) !== false;  $n++) {
+	   echo "\nDROP TABLE IF EXISTS $table;";
+	   echo "\nCREATE TABLE $table (issn int not null primary key, issn_l int not null);";
+	   echo "\nINSERT INTO $table (issn, issn_l) VALUES ";
+	   for($n=0; (!$MAX || $n<$MAX) && ($line = fgets($handle, 4096)) !== false;  $n++) {
 	    	preg_match('/^\s*(\d+)\-(\d{3,3})[X\d]\s(\d+)\-(\d{3,3})[X\d]$/i',$line,$m);
 	        if (isset($m[1])) {
 	        	echo "$sep\n($m[1]$m[2],$m[3]$m[4])";
@@ -39,17 +35,17 @@ function echo_fileFiltered(
 	        	echo ";\n\nINSERT INTO $table (issn, issn_l) VALUES ";
 	        	$sep='';	        	
 	        }
-	    }
-	    if ((!$MAX || $n!=$MAX)  && !feof($handle)) {
+	   } // for
+	   if ((!$MAX || $n!=$MAX)  && !feof($handle)) {
 	        echo "\n##Error: unexpected fgets() fail\n";
-	    } else 
+	   } else 
 	    	echo ";";
-	    fclose($handle);
-	}	
-}
+	   fclose($handle);
+	} // if	
+} // func
 
 if (isset($argv[0])) {
-	// cuisado: usar comando shell
+	// alert: using at terminal (shell)
 	$n = isset($argv[1])? 0: 20;
 	echo_fileFiltered('issnltables', $n, 'lib.issn_l',90000);
 }
