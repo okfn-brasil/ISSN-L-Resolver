b1ea177208ac0e755fdd85fe68df7212d6584256
ppkrauss <ppkrauss@gmail.com>
2014-08-19 08:44:21 -0300

    review

--- webservice/index.php
+++ webservice/index.php
@@ -1,26 +1,8 @@
 <?php
 /**
- * Conversor de arquivo ISSN-to-ISSNL para SQL PostgresSQL.
+ * ...UNDER CONSTRUCTION!!
  */
 include('issnltables2sql.php');
 
+// ...
 ?>
-<html>
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>ISSN-to-ISSNL </title>
-  </head>
-<body>
-
-Teste por amostragem: 
-
-<textarea rows="20" cols="120"><?php
-	echo_fileFiltered();
-?>
-</textarea>
-Para usar, confira o "issnltables2sql.php" ou https://github.com/ppKrauss/ISSN-L-resolver
-
-
-</body>
-</html>
-
