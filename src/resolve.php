ad213518188ca584f8fd2ed18b78e39585c44741
User <ppkrauss@gmail.com>
2017-06-28 15:17:47 -0300

    debug web

--- src/resolve.php
+++ src/resolve.php
@@ -1,40 +1,46 @@
 <?php
 /**
  * Run as web-service or terminal command. Requires PHP v7.1.
- * Commands  N2C,N2Ns,isN,isC,info, list
- * php resolve.php -j --n2n 1234567
- * http://localhost/gits/ISSN-L-resolver/webservice/
+ * Commands  N2C,N2Ns,isN,isC,info, list, ...
+ * By terminal:
+ *   php resolve.php -j --n2n 1234567
+ * By Web:
+ * http://teste.oficial.news/resolve.php?opname=n2c&sval=1
  */
 
 include('conf.php');
 
 if ($is_cli) {
   $optind = null;
-  $opts = getopt('hjx', $cmdValid, $optind); // exige PHP7.1
+  $opts = getopt('hjxd', $cmdValid, $optind); // exige PHP7.1  -d=debug
   $extras = array_slice($argv, $optind);
   $outFormat = isset($opts['x'])? 'x': 'j';  // x|j|t
   unset($opts['x']);unset($opts['j']);
   $cmd = array_keys($opts);
   if (isset($opts['h']) || !count($extras) || count($cmd)!=1)
-    die("\n---- ISSN-L RESOLVER -----".file_get_contents('synopsis.txt')." \n");
+    die("\n---- ISSN-L RESOLVER -----\n".file_get_contents('synopsis.txt')." \n");
   $sval = $extras[0];
   $opname = strtolower(trim($cmd[0]));
   $outType   = 'int';
-
+  $debug =0;
 } else {
  	$opname = isset($_GET['opname'])? strtolower(trim($_GET['opname'])): '';
  	$sval = isset($_GET['sval'])? trim($_GET['sval']): ''; 				// string input
  	$outFormat = isset($_GET['format'])? trim($_GET['format']): 'x'; // h|x|j|t
  	$outType   = 'int';
+  $debug = isset($_GET['debug'])? 1: 0;
 }
 
-if (!$is_cli) {
-	if ($status!=200) http_response_code($status);
+if (!$is_cli && !$debug) {
+  if ($status!=200) http_response_code($status);
 	header("Content-Type: $outFormatMime[$outFormat]");
 }
 
 $output = issnLresolver($opname,$sval,$outType,$outFormat);
-echo json_encode($output);
+if ($debug)
+  echo "issnLresolver($opname,$sval,$outType,$outFormat) = [status $status, out $output]";
+else
+  echo json_encode($output);
 
 
 //////////////////// LIB ////////////////////
