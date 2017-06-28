4b571d11b513d54610d3ec11e9a1ddc3022ad56b
User <ppkrauss@gmail.com>
2017-06-28 14:15:58 -0300

    ops

--- src/conf.php
+++ src/conf.php
@@ -1,16 +1,18 @@
 <?php
-//CONF
+/**
+ * Basic and default configurations.
+ */
+
 $PG_CONSTR = 'pgsql:host=localhost;port=5432;dbname=issnl';
 $PG_USER = 'postgres';
 $PG_PW   = 'postgres';
 
-$is_cli = is_cli();
+$is_cli = (php_sapi_name() === 'cli');  // true when is client (terminal).
 
-/**
- * Check if is terminal or not.
- * @return boolean true when is client (terminal).
- */
-function is_cli() { return (php_sapi_name() === 'cli');  }
+$outFormatMime = ['j'=>'application/json', 'x'=>'application/xml', 't'=>'text/plain'];
+$status = 200;
+  // 404 - has not found the input issn.
+  // 416 - issn format is invalid.
 
+$cmdValid = ['N2N','N2Ns','N2C','N2Cs','N2U','N2Us','isN','isC','info', 'infodb'];
 ?>
-
